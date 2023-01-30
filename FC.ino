a3c9bb6672917cd20ed8e2ec6be691e723e4a967
Joshua Caudill <56001713+joshuacaudill60@users.noreply.github.com>
2023-01-30 11:17:05 -0500

    Create FC.ino

---
+++ FC.ino
@@ -0,0 +1,15 @@
+uint8_t spec_data;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial1.begin(9600);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (Serial1.available()) {
+    spec_data = Serial1.read();
+    Serial.print(spec_data);
+  }
+}
